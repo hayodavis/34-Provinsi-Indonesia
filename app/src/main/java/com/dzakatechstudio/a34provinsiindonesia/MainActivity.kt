49cefaf904591561f1f33d300b0fb7ddb376252d
Davis <davis.fandi@gmail.com>
2021-06-29 06:11:41 +0700

    Create project and setting module.app

---
+++ app/src/main/java/com/dzakatechstudio/a34provinsiindonesia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dzakatechstudio.a34provinsiindonesia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
