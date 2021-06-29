dd19a1852f2cb998f35d05793d4a80af0457a7fc
Davis <davis.fandi@gmail.com>
2021-06-29 09:46:42 +0700

    menambahkan data class

---
+++ app/src/main/java/com/dzakatechstudio/a34provinsiindonesia/Province.kt
@@ -0,0 +1,8 @@
+package com.dzakatechstudio.a34provinsiindonesia
+
+data class Province(
+    var name: String = "",
+    var photo: String = "",
+    var detail: String = "",
+    var deskripsi: String = ""
+)
